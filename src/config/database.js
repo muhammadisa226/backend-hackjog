eefbd24052a5b50a8d31aed46b026a0b166f0e66
muhammadisa226 <muhammadisa226@gmail.com>
2023-10-19 22:30:50 +0700

    change readme.md

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,12 @@ const db = new Sequelize("hackjog", "root", "", {
   host: "127.0.0.1",
   dialect: "mysql",
   timezone: "+07:00",
+  port: "3306",
 });
 // const db = new Sequelize("dbname", "dbuser", "dbpassword", {
 //   host: "127.0.0.1",
 //   dialect: "mysql",
 //   timezone: "+07:00",
+// port: "dbport",
 // });
 module.exports = { db };
