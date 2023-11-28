676957ac8e6f00e86fb19022cded0b37df37c8c1
muhammadisa226 <muhammadisa226@gmail.com>
2023-11-28 11:28:03 +0700

    add public folder

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,14 @@
-const { Sequelize } = require("sequelize");
-const db = new Sequelize("hackjog", "root", "", {
-  host: "127.0.0.1",
+const { Sequelize } = require("sequelize")
+const db = new Sequelize("hackjog", "muhammadisa", "muhammadisa", {
+  host: "172.19.57.178",
   dialect: "mysql",
   timezone: "+07:00",
-  port: "3306",
-});
+  port: "3307",
+})
 // const db = new Sequelize("dbname", "dbuser", "dbpassword", {
 //   host: "127.0.0.1",
 //   dialect: "mysql",
 //   timezone: "+07:00",
 // port: "dbport",
 // });
-module.exports = { db };
+module.exports = { db }
